d8854cb4fd781366259649ba66193ed04a5e15c9
Uri Gorodetsky <urikan76@gmail.com>
2021-10-18 01:10:45 +0300

    changed

--- TestCalc/BasicOpTests.cs
+++ TestCalc/BasicOpTests.cs
@@ -28,7 +28,7 @@ namespace TestCalc
         [TestMethod]
         public void DivideTest()
         {
-            Assert.AreEqual(3, Program.DivideNumbers(4, 2));
+            Assert.AreEqual(4, Program.DivideNumbers(4, 2));
         }
     }
 }
