44d74196095e1e4b190929632eeb77f8c774edf7
Uri Gorodetsky <urikan76@gmail.com>
2021-10-22 07:59:29 +0300

    Break add test

--- TestCalc/BasicOpTests.cs
+++ TestCalc/BasicOpTests.cs
@@ -10,7 +10,7 @@ namespace TestCalc
         [TestMethod]
         public void AddTest()
         {
-            Assert.AreEqual(43,Program.AddNumbers(3,40));
+            Assert.AreEqual(33,Program.AddNumbers(3,40));
         }
 
         [TestMethod]
